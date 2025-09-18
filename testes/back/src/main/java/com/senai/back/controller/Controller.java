bf3ac2a2ed4cc508a36296acece7fc322c2577d6
2DM <weysrigah@gmail.com>
2025-09-18 09:08:06 -0300

    feat

---
+++ testes/back/src/main/java/com/senai/back/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.senai.back.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping
+	public String oi() {
+		return "Olá, raparigo";
+	}
+}
